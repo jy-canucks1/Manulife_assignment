65ac731c24a368f878882f4b4ccbe63ee805542f
JaiYeon_Choi <84373345+jy-canucks1@users.noreply.github.com>
2022-01-18 15:14:20 +0900

    Add files via upload

---
+++ reactJS_part/src/App.js
@@ -0,0 +1,15 @@
+import GameList from './components/GameList' 
+import PlatformList from './components/PlatformListRest';
+import './App.css';
+
+function App(props) {
+  return (
+  <>
+  <h1>Team 115's Awesome Assignment</h1>
+  <GameList/>
+  <PlatformList/>
+  </>
+  )
+}
+//
+export default App;
